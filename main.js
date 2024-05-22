57f3507e9095554f12812a660b015591e88275c9
grigoryan-m <waldm4rk@gmail.com>
2024-05-22 17:42:09 +0400

    changed x to y;

--- main.js
+++ main.js
@@ -17,53 +17,53 @@ document.addEventListener("DOMContentLoaded", ()=>{
     function calculateCoords(id){
     	let result = "";
 
-	let x = id.split("-")[1];
-	let y = id.split("-")[0];
-	let tempY = 0;
+	let x = id.split("-")[0];
+	let y = id.split("-")[1];
+	let tempX = 0;
 	// AA-001
-	if(y.charAt(0) == "A"){
-		tempY = 0;
-	}else if(y.charAt(0) == "B"){
-		tempY = 26;
-	}else if(y.charAt(0) == "C"){
-		tempY = 52;
-	}else if(y.charAt(0) == "D"){
-		tempY = 78;
-	}else if(y.charAt(0) == "E"){
-		tempY = 104;
-	}else if(y.charAt(0) == "F"){
-		tempY = 130;
-	}else if(y.charAt(0) == "G"){
-		tempY = 156;
-	}else if(y.charAt(0) == "H"){
-		tempY = 182;
-	}else if(y.charAt(0) == "I"){
-		tempY = 208;
-	}else if(y.charAt(0) == "J"){
-		tempY = 234;
-	}else if(y.charAt(0) == "K"){
-		tempY = 260;
-	}else if(y.charAt(0) == "L"){
-		tempY = 286;
-	}else if(y.charAt(0) == "M"){
-		tempY = 312;
-	}else if(y.charAt(0) == "N"){
-		tempY = 338;
-	}else if(y.charAt(0) == "O"){
-		tempY = 364;
-	}else if(y.charAt(0) == "P"){
-		tempY = 390;
+	if(x.charAt(0) == "A"){
+		tempX = 0;
+	}else if(x.charAt(0) == "B"){
+		tempX = 26;
+	}else if(x.charAt(0) == "C"){
+		tempX = 52;
+	}else if(x.charAt(0) == "D"){
+		tempX = 78;
+	}else if(x.charAt(0) == "E"){
+		tempX = 104;
+	}else if(x.charAt(0) == "F"){
+		tempX = 130;
+	}else if(x.charAt(0) == "G"){
+		tempX = 156;
+	}else if(x.charAt(0) == "H"){
+		tempX = 182;
+	}else if(x.charAt(0) == "I"){
+		tempX = 208;
+	}else if(x.charAt(0) == "J"){
+		tempX = 234;
+	}else if(x.charAt(0) == "K"){
+		tempX = 260;
+	}else if(x.charAt(0) == "L"){
+		tempX = 286;
+	}else if(x.charAt(0) == "M"){
+		tempX = 312;
+	}else if(x.charAt(0) == "N"){
+		tempX = 338;
+	}else if(x.charAt(0) == "O"){
+		tempX = 364;
+	}else if(x.charAt(0) == "P"){
+		tempX = 390;
 	}
 
-	result += parseInt(x) + "; ";
+	
 	const alphabet = [];
         for (let i = 65; i <= 90; i++) {
             	alphabet.push(String.fromCharCode(i));
         }
- 	tempY += alphabet.indexOf(y.charAt(1));
+ 	tempX += alphabet.indexOf(x.charAt(1));
 
-	result += tempY;
-
+	result += tempX;
+	result += "; " + parseInt(y);
 	return result;
     }
 
@@ -71,54 +71,54 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     function calculateID(x, y){
         let result = "";
-        if (y >= 0 && y <= 25) {
+        if (x >= 0 && x <= 25) {
             result += "A";
-            y -= 0;
-        } else if (y >= 26 && y <= 51) {
+            x -= 0;
+        } else if (x >= 26 && x <= 51) {
             result += "B";
-            y -= 26;
-        } else if (y >= 52 && y <= 77) {
+            x -= 26;
+        } else if (x >= 52 && x <= 77) {
             result += "C";
-            y -= 52;
-        } else if (y >= 78 && y <= 103) {
+            x -= 52;
+        } else if (x >= 78 && x <= 103) {
             result += "D";
-            y -= 78;
-        } else if (y >= 104 && y <= 129) {
+            x -= 78;
+        } else if (x >= 104 && x <= 129) {
             result += "E";
-            y -= 104;
-        } else if (y >= 130 && y <= 155) {
+            x -= 104;
+        } else if (x >= 130 && x <= 155) {
             result += "F";
-            y -= 130;
-        } else if (y >= 156 && y <= 181) {
+            x -= 130;
+        } else if (x >= 156 && x <= 181) {
             result += "G";
-            y -= 156;
-        } else if (y >= 182 && y <= 207) {
+            x -= 156;
+        } else if (x >= 182 && x <= 207) {
             result += "H";
-            y -= 182;
-        } else if (y >= 208 && y <= 233) {
+            x -= 182;
+        } else if (x >= 208 && x <= 233) {
             result += "I";
-            y -= 208;
-        } else if (y >= 234 && y <= 259) {
+            x -= 208;
+        } else if (x >= 234 && x <= 259) {
             result += "J";
-            y -= 234;
-        } else if (y >= 260 && y <= 285) {
+            x -= 234;
+        } else if (x >= 260 && x <= 285) {
             result += "K";
-            y -= 260;
-        } else if (y >= 286 && y <= 311) {
+            x -= 260;
+        } else if (x >= 286 && x <= 311) {
             result += "L";
-            y -= 286;
-        } else if (y >= 312 && y <= 337) {
+            x -= 286;
+        } else if (x >= 312 && x <= 337) {
             result += "M";
-            y -= 312;
-        } else if (y >= 338 && y <= 363) {
+            x -= 312;
+        } else if (x >= 338 && x <= 363) {
             result += "N";
-            y -= 338;
-        } else if (y >= 364 && y <= 389) {
+            x -= 338;
+        } else if (x >= 364 && x <= 389) {
             result += "O";
-            y -= 364;
-        } else if (y >= 390 && y <= 415) {
+            x -= 364;
+        } else if (x >= 390 && x <= 415) {
             result += "P";
-            y -= 390;
+            x -= 390;
         }
         
         const alphabet = [];
@@ -126,14 +126,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
             alphabet.push(String.fromCharCode(i));
         }
 
-        result += alphabet[y] + "-";
+        result += alphabet[x] + "-";
 
-        if(x < 10){
-            result += "00" + x;
-        }else if(x < 100){
-            result += "0" + x;
+        if(y < 10){
+            result += "00" + y;
+        }else if(y < 100){
+            result += "0" + y;
         }else{
-            result += x;
+            result += y;
         }
 
         return result;
